885040985e9b462cc5f7b4e73cbb65ff7fec661d
Kondrev Alexander Dmitrievich <akondrev@ozon.ru>
2021-09-20 21:24:54 +0300

    Init repository

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
+
+import Vue from 'vue';
+import App from './ui/templates/App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
